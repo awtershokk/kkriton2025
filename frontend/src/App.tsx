f16790751bcaa8dfe5deeaa81cc365aef51bb729
grishakuznetsov <awtershokk@gmail.com>
2025-04-27 14:45:38 +0700

    базовая структура + настройка фронтенда

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-black">
+            <h1 className="text-4xl font-black">скоро тут будет жесточайший проект на хакатон...</h1>
+        </div>
+    )
+}
+
+export default App
