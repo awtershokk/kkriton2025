aabcf51c759d17eeabdc3d3c037c0667f5a182f6
grishakuznetsov <awtershokk@gmail.com>
2025-04-27 15:14:50 +0700

    адаптив для заглушки

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,10 @@ import './App.css'
 
 function App() {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-black">
-            <h1 className="text-4xl font-black">скоро тут будет жесточайший проект на хакатон...</h1>
+        <div className="flex items-center justify-center min-h-screen bg-black p-4">
+            <h1 className="text-2xl sm:text-3xl md:text-4xl font-black text-center text-white">
+                скоро тут будет жесточайший проект на хакатон...
+            </h1>
         </div>
     )
 }
