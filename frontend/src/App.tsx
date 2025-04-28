27975c4ff95f5f13b9554adc9582f0472a5eca11
grishakuznetsov <awtershokk@gmail.com>
2025-04-28 18:08:19 +0700

    сайдбар

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/HomePage/HomePage.tsx";
+import LoginForm from "./pages/Auth/LoginPage.tsx";
+import RegForm from "./pages/Auth/RegisterPage.tsx";
+import HelpPage from "./pages/HelpPage/HelpPage.tsx";
 
 const App = () => {
 
     return (
-            <Router>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                </Routes>
-            </Router>
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/register" element={<RegForm />} />
+                <Route path="/help" element={<HelpPage />} />
+            </Routes>
+        </Router>
     );
 };
 
