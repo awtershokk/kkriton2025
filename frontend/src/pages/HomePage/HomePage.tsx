3528084ab82efd357581ea13fef1232589837e5c
grishakuznetsov <awtershokk@gmail.com>
2025-04-28 12:36:58 +0700

    левая часть главной страницы

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import backgroundImg from "../../assets/background /veterans.jpg";
+import backgroundImg from "../../assets/background/veterans.jpg";
 
 const HomePage: React.FC = () => {
     return (
         <div
             className="h-screen w-screen bg-cover bg-center bg-no-repeat"
-            style={{backgroundImage: `url(${backgroundImg})`}}
+            style={{ backgroundImage: `url(${backgroundImg})` }}
         >
-            <div className="p-[120px] h-calc(100%-200px) flex justify-start">
-                <div className="w-[600px]">
-                    <h1 className="text-[36px] font-bold break-words">
+            <div className="flex flex-col justify-center h-full px-[110px]">
+                <div className="w-[600px] mx-auto">
+                    <h1 className="text-[46px] font-bold break-words">
                         Платформа для помощи ветеранам и семьям военнослужащих
                     </h1>
+                    <h2 className="text-[23px] break-words mt-[10px]">
+                        Платформа направлена на решение проблем в области медицины, юриспруденции и социальной помощи,
+                        облегчая жизнь тем, кто отдал много ради нашей безопасности.
+                    </h2>
+                    <button
+                        className="w-[300px] bg-[rgba(233,81,0,0.8)] text-[23px] hover:bg-[#E95100] py-2 mt-[20px] rounded"
+                    >
+                        Поддержать проект
+                    </button>
                 </div>
             </div>
         </div>
