82bc6e5a887c404394e16578677b1cfb2a1372d5
grishakuznetsov <awtershokk@gmail.com>
2025-04-29 11:04:07 +0700

    новая модель для меропрития

--- backend/internal/database/postgres/models/event.go
+++ backend/internal/database/postgres/models/event.go
@@ -7,11 +7,17 @@ import (
 
 type Event struct {
 	gorm.Model
-	StartTime   time.Time `gorm:"column:start_time;not null"`
-	EndTime     time.Time `gorm:"column:end_time;not null"`
-	Location    string    `gorm:"column:location;type:text;not null"`
-	Purpose     string    `gorm:"column:purpose;type:text"`
-	Description string    `gorm:"column:description;type:text"`
+	StartTime    time.Time `gorm:"column:start_time;not null"`
+	EndTime      time.Time `gorm:"column:end_time;not null"`
+	Location     string    `gorm:"column:location;type:text;not null"`
+	Purpose      string    `gorm:"column:purpose;type:text"`
+	Description  string    `gorm:"column:description;type:text"`
+
+	NKOID       *uint      `gorm:"column:nko_id"`
+	NKO         *NKO       `gorm:"foreignKey:NKOID"`
+
+	VolunteerID *uint      `gorm:"column:volunteer_id"`
+	Volunteer   *Volunteer `gorm:"foreignKey:VolunteerID"`
 }
 
 func (Event) TableName() string {
